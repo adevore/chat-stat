a8e2b4e64d6e04fce7444b1c90c9d58725e26ca0
Aaron DeVore <aaron.devore@gmail.com>
2011-01-30 15:49:21 -0800

    Support for creating an index.html for counters

---
+++ web_compile.py
@@ -0,0 +1,64 @@
+import optparse
+import os, sys
+from os.path import dirname, expanduser, exists, abspath
+
+import jinja2
+
+from jinja2 import Environment, PackageLoader, Template
+
+from chatstatlib import unpackJSON
+
+TEMPLATE_NAME = "index-template.html"
+
+USAGE = "%prog [options] <target config> <stats config>"
+options = optparse.OptionParser(usage=USAGE)
+options.add_option("-o", "--outfile", default="-", dest="outfile",
+    help="Output file (\"-\" for stdout, defaults to stdout)")
+
+
+def checkPaths(target, stats, outfile):
+    if not exists(target):
+        print "Config for targets '%s' does not exist" % target
+        exit(1)
+    elif not exists(stats):
+        print "Config for stats '%s' does not exist" % stats
+        exit(1)
+    elif outfile:
+        outdir = dirname(abspath(expanduser(outfile)))
+        if not exists(outdir):
+            print "directory for out file '%s' does not exist" % outfile
+
+
+def compileTemplate(tree):
+    # TODO Set up PackageLoader
+    loader = PackageLoader('chatstatlib', 'templates')
+    env = Environment(loader=loader)
+
+    template = env.get_template(TEMPLATE_NAME)
+    return template.render(channels=tree.channels, stats=tree.stats)
+
+
+
+
+def main():
+    opts, args = options.parse_args()
+    if len(args) not in (2, 3):
+        options.print_help()
+        exit(1)
+    targetConfigPath = expanduser(args[0])
+    statsConfigPath = expanduser(args[1])
+    if len(args) == 3:
+        outfile = args[2]
+    else:
+        outfile = None
+    checkPaths(targetConfigPath, statsConfigPath, outfile)
+    if outfile:
+        out = open(outfile, 'w')
+    else:
+        out = sys.stdout
+    tree = unpackJSON(targetConfigPath, statsConfigPath)
+    html = compileTemplate(tree)
+    out.write(html)
+
+if __name__ == "__main__":
+    main()
