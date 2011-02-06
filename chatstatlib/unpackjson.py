1fcaa1b583bae6b096c1c15848217d8440549349
Aaron DeVore <aaron.devore@gmail.com>
2011-02-06 02:03:20 -0800

    switch JSON string type checking to basestring

--- chatstatlib/unpackjson.py
+++ chatstatlib/unpackjson.py
@@ -26,7 +26,7 @@ def jsonKey(node, key, type=None, default=None):
             raise FormatError("node {0} does not have child {1}".format(node, key))
     value = node[key]
     if type and not isinstance(value, type):
-        raise FormatError("node {0} is not a '{1}'".format(node, type.__name__))
+        raise FormatError("node {0} of type '{1}' is not a '{2}'".format(value, value.__class__.__name__, type.__name__))
     return value
 
 
@@ -36,20 +36,20 @@ class Channel(object):
     """
 
     def __init__(self, parent, subtree):
-        self.id = jsonKey(subtree, 'id', type=unicode)
-        self.label = jsonKey(subtree, 'label', type=unicode)
-        self.source = jsonKey(subtree, 'src', default="", type=unicode)
+        self.id = jsonKey(subtree, 'id', type=basestring)
+        self.label = jsonKey(subtree, 'label', type=basestring)
+        self.source = jsonKey(subtree, 'src', default="", type=basestring)
         self.network = jsonKey(subtree, 'network',
-                       default=parent.network, type=unicode)
+                       default=parent.network, type=basestring)
 
 
 class Statistic(object):
     def __init__(self, parent, subtree):
         self.limits = jsonKey(subtree, 'limits',
                       default=parent.defaultLimits, type=list)
-        self.label = jsonKey(subtree, 'label', type=unicode)
-        self.id = jsonKey(subtree, 'id', type=unicode)
-        self.counter = jsonKey(subtree, 'counter', type=unicode)
+        self.label = jsonKey(subtree, 'label', type=basestring)
+        self.id = jsonKey(subtree, 'id', type=basestring)
+        self.counter = jsonKey(subtree, 'counter', type=basestring)
         self.args = jsonKey(subtree, 'args', default=[], type=list)
         self.kwargs = jsonKey(subtree, 'kwargs', default={}, type=dict)
 
@@ -66,9 +66,10 @@ class Orders(object):
         assertJSON(targetJSON, type=dict)
         self.defaultLimits = jsonKey(targetJSON, 'limits',
                              default=[0], type=list)
-        self.format = jsonKey(targetJSON, 'format', type=unicode)
+        self.format = jsonKey(targetJSON, 'format', type=basestring)
         self.network = jsonKey(targetJSON, 'network',
-                       default=None, type=unicode)
+                       default=None, type=basestring)
+        self.sort = jsonKey(targetJSON, 'sort', type=bool, default=False)
         self.channels = [Channel(self, target)
             for target in jsonKey(targetJSON, 'targets', type=list)]
 
